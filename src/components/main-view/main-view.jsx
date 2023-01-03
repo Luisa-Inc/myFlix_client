ec0d76c467d3884996f373d36c4d527600e2e75e
Luisa Ihle <luisa.ihle@gmail.com>
2023-01-03 11:04:20 +0100

    Update main-view.jsx
    update image paths

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,7 +8,7 @@ export const MainView = () => {
       id: 1,
       title: "Silence of the Lambs",
       image:
-        "https://www.imdb.com/title/tt0102926/mediaviewer/rm3533075712/?ref_=tt_ov_i",
+        "https://www.themoviedb.org/t/p/original/9fSjFdLxyVFbSySTIwKwOhxlIOI.jpg",
       director: "Jonathan Demme",
       genre: "Thriller",
       description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer."
@@ -17,7 +17,7 @@ export const MainView = () => {
       id: 2,
       title: "The Lion King",
       image:
-      "https://www.imdb.com/title/tt0110357/mediaviewer/rm3169350912/?ref_=tt_ov_i",
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzK5hNoRPkiTZdYpWMCiD5J5pwMnLVxnMCKftPXcOj5i0gouCb",
       director: "Rob Minkoff",
       genre: "Animated",
       description: "This Disney animated feature follows the adventures of the young lion Simba."
@@ -26,7 +26,7 @@ export const MainView = () => {
       id: 3,
       title: "Stuart Little",
       image:
-        "https://www.imdb.com/title/tt0164912/mediaviewer/rm2978165504/?ref_=tt_ov_i",
+        "https://www.themoviedb.org/t/p/w500/niZSfupE9XR7GmEUq2XdURVH66T.jpg",
       director: "Rob Minkoff",
       genre: "Comedy",
       description: "When the Littles go to an orphanage to adopt a new family member, a charming young mouse named Stuart is chosen."
@@ -35,7 +35,7 @@ export const MainView = () => {
       id: 4,
       title: "Knocked Up",
       image:
-        "https://www.imdb.com/title/tt0478311/mediaviewer/rm2982937088/?ref_=tt_ov_i",
+        "https://www.themoviedb.org/t/p/original/ad8w2i0mwXS3BzcvUzinnWC5BlP.jpg",
       director: "Judd Apatow",
       genre: "Comedy",
       description: "Ambitious Los Angeles reporter Alison Scott has just been given an on-air role with E! and lives in the guest house of her sister Debbie's family."
@@ -44,7 +44,7 @@ export const MainView = () => {
       id: 5,
       title: "Swimming to Cambodia",
       image:
-        "https://www.imdb.com/title/tt0094089/mediaviewer/rm768029696/?ref_=tt_ov_i",
+        "https://www.themoviedb.org/t/p/w500/7AUoZQaWpRHzgyDntvTBVMggGzo.jpg",
       director: "Jonathan Demme",
       genre: "Animated",
       description: "Swimming to Cambodia, also known as Spalding Gray's Swimming to Cambodia, is a 1987 American concert film. It was nominated for Best Feature, Best Director, Best Actor, and Best Screenplay at the 1988 Independent Spirit Awards."
