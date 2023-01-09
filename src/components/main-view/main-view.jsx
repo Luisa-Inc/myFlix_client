082e3e1d739dcd2af480e62c2bc4c44a8cdf8d26
Luisa-Inc <118922382+Luisa-Inc@users.noreply.github.com>
2023-01-09 14:48:55 +0100

    Add files via upload

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import React from 'react';
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([
@@ -8,7 +11,7 @@ export const MainView = () => {
       id: 1,
       title: "Silence of the Lambs",
       image:
-        "https://www.themoviedb.org/movie/274-the-silence-of-the-lambs?language=de#",
+        "https://www.themoviedb.org/t/p/original/9fSjFdLxyVFbSySTIwKwOhxlIOI.jpg",
       director: "Jonathan Demme",
       genre: "Thriller",
       description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer."
@@ -17,7 +20,7 @@ export const MainView = () => {
       id: 2,
       title: "The Lion King",
       image:
-      "https://www.themoviedb.org/movie/8587-the-lion-king?language=de#",
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzK5hNoRPkiTZdYpWMCiD5J5pwMnLVxnMCKftPXcOj5i0gouCb",
       director: "Rob Minkoff",
       genre: "Animated",
       description: "This Disney animated feature follows the adventures of the young lion Simba."
@@ -26,7 +29,7 @@ export const MainView = () => {
       id: 3,
       title: "Stuart Little",
       image:
-        "https://www.themoviedb.org/movie/10137-stuart-little?language=de#",
+        "https://www.themoviedb.org/t/p/w500/niZSfupE9XR7GmEUq2XdURVH66T.jpg",
       director: "Rob Minkoff",
       genre: "Comedy",
       description: "When the Littles go to an orphanage to adopt a new family member, a charming young mouse named Stuart is chosen."
@@ -35,7 +38,7 @@ export const MainView = () => {
       id: 4,
       title: "Knocked Up",
       image:
-        "https://www.themoviedb.org/movie/4964-knocked-up?language=de#",
+        "https://www.themoviedb.org/t/p/original/ad8w2i0mwXS3BzcvUzinnWC5BlP.jpg",
       director: "Judd Apatow",
       genre: "Comedy",
       description: "Ambitious Los Angeles reporter Alison Scott has just been given an on-air role with E! and lives in the guest house of her sister Debbie's family."
@@ -44,7 +47,7 @@ export const MainView = () => {
       id: 5,
       title: "Swimming to Cambodia",
       image:
-        "https://www.themoviedb.org/movie/42015-swimming-to-cambodia?language=de",
+        "https://www.themoviedb.org/t/p/w500/7AUoZQaWpRHzgyDntvTBVMggGzo.jpg",
       director: "Jonathan Demme",
       genre: "Animated",
       description: "Swimming to Cambodia, also known as Spalding Gray's Swimming to Cambodia, is a 1987 American concert film. It was nominated for Best Feature, Best Director, Best Actor, and Best Screenplay at the 1988 Independent Spirit Awards."
@@ -53,18 +56,95 @@ export const MainView = () => {
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    fetch("https://mighty-harbor-05233.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const moviesFromApi = data.map((doc) => {
+          return {
+            id: doc._id,
+            title: doc.Title,
+            image: doc.ImagePath,
+            director: doc.Director,
+            genre: doc.Genre,
+            description: doc.Description,
+          };
+        });
+      
+        setMovies(moviesFromApi);
+      
+      });
+  }, [token]);
+
+
+
+if (!user) {
+  return (
+    <>
+      <LoginView onLoggedIn={(user, token) => {
+        setUser(user);
+        setToken(token);
+      }} />
+      or
+      <SignupView />
+    </>
+  );
+}
   if (selectedMovie) {
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setUser(null);
+            setToken(null)
+          }}
+        >
+          Logout
+        </button>
+        <MovieView
+          movie={selectedMovie}
+          onBackClick={() => setSelectedMovie(null)}
+        />
+      </>
     );
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return (
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear()
+          }}
+        >
+          Logout
+        </button>
+        <div>The list is empty!</div>
+      </>
+    );
   }
 
   return (
     <div>
+      <button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear()
+        }}
+      >
+        Logout
+      </button>
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,34 @@
+// Here you import the PropTypes library
+import PropTypes from "prop-types";
+
+// The MovieCard function component 
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
-    );
-  };
-  
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
+  );
+};
+
+// Here is where we define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string,
+      bio: PropTypes.string,
+      birth: PropTypes.number
+    }),
+    genre: PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string
+    }),
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
