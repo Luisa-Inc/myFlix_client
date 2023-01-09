61c8f979ee69c34df3acb1e34b12708dc68b9478
Luisa Ihle <luisa.ihle@gmail.com>
2023-01-09 10:06:28 +0100

    update main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,55 +5,54 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 
-export const MainView() {
+export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
   const storedToken = localStorage.getItem("token");
-  const [user, setUser] = useState(storedUser? storedUser : null);
-  const [token, setToken] = useState(storedToken? storedToken : null);
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    if (!token) 
+    if (!token) {
       return;
+    }
 
     fetch("https://mighty-harbor-05233.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${token}` }
     })
-    .then((response) => response.json())
-    .then((movies) => {
-      setMovies(movies);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, [token]);
 
-    });
-}, [token]);
+  const moviesFromApi = data.map((doc) => {
+    return {
+      id: doc._id,
+      title: doc.Title,
+      image: doc.ImagePath,
+      director: doc.Director,
+      genre: doc.Genre,
+      description: doc.Description,
+    };
+  });
 
-        const moviesFromApi = data.map((doc) => {
-          return {
-              id: doc._id,
-              title: doc.Title,
-              image: doc.ImagePath,
-              director: doc.Director,
-              genre: doc.Genre,
-              description: doc.Description,
-              }
-        });
+  setMovies(moviesFromApi);
+} ;
 
-        setMovies(moviesFromApi);
-  } ;
-
-  if (!user) {
-    return (
-      <>
-        <LoginView onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }} />
-        or
-        <SignupView />
-      </>
-    );
-  }
-
+if (!user) {
+  return (
+    <>
+      <LoginView onLoggedIn={(user, token) => {
+        setUser(user);
+        setToken(token);
+      }} />
+      or
+      <SignupView />
+    </>
+  );
+}
   if (selectedMovie) {
     return (
       <>
