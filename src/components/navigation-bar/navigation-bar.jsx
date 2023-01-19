65f27e63ee59ce62f5e552cbb7529effae666635
Luisa Ihle <luisa.ihle@gmail.com>
2023-01-19 19:59:27 +0100

    update navigation-bar

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,7 +1,5 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
 
 import "./navigation-bar.scss";
 
