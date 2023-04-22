40f8f7c79a5201d43bfd9044846c43c4bbc7fcbf
Luisa Ihle <luisa.ihle@gmail.com>
2023-04-22 17:17:24 +0200

    Update navbar
    reduced visibilityfilter

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -3,6 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import Form from "react-bootstrap/Form";
+import {visibilityFilter} from 
 
 import "./navigation-bar.scss";
 
